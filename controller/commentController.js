b30ee41d1c9e3f1657d0e41be0742841e0b73cf7
Siddharth Pande <sidarjunstn.sp@gmail.com>
2023-03-13 20:05:00 +0530

    comment controller

---
+++ controller/commentController.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+exports.comment_get = (req, res, next) => {
+  res.send("NOT IMPLEMENTED: show comments GET");
+};
+
+exports.comment_post = (req, res, next) => {
+  res.send("NOT IMPLEMENTED: write comment POST");
+};
--- routes/public.js
+++ routes/public.js
@@ -2,8 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const bpostController = require("../controller/bpostController");
+const commentController = require("../controller/commentController");
 
 //Show all blog-posts
 router.get("/bpost", bpostController.bpost_get);
+
+//Show comments in a particular blog post
+router.get("/bpost/:id/comment", commentController.comment_get);
+
+//Write comments on a particular blog post
+router.post("/bpost/:id/comment", commentController.comment_post);
 
 module.exports = router;
