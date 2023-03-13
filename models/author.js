436bb3a894e28632954c88b174a6bd89d202c19d
Siddharth Pande <sidarjunstn.sp@gmail.com>
2023-03-13 17:10:39 +0530

    Author model created and exported

---
+++ models/author.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+  name: { type: String, required: true, minLength: 1 },
+  username: { type: String, required: true, minLength: 1 },
+  password: { type: String, required: true, minLength: 1 },
+});
+
+module.exports = mongoose.model("AUTHOR", AuthorSchema);
