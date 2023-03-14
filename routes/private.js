7d935e7e5948eae66993285a672e53e70fbc34ad
Siddharth Pande <sidarjunstn.sp@gmail.com>
2023-03-14 17:57:10 +0530

    puged the private.js file

--- routes/private.js
+++ routes/private.js
@@ -4,24 +4,12 @@ const router = express.Router();
 const bpostController = require("../controller/bpostController");
 
 //Create Blog-post (works only when authenticated)
-router.post(
-  "/bpost",
-  "passport.authenticate_in_here",
-  bpostController.bpost_post
-);
+router.post("/bpost", bpostController.bpost_post);
 
 //Publish the unpublished posts
-router.put(
-  "/bpost/:id/publish",
-  "passport.authenticate_in_here",
-  bpostController.bpost_put_publish
-);
+router.put("/bpost/:id/publish", bpostController.bpost_put_publish);
 
 //Unpublish the published posts
-router.put(
-  "/bpost/:id/unpublish",
-  "passport.authenticate_in_here",
-  bpostController.bpost_put_unpublish
-);
+router.put("/bpost/:id/unpublish", bpostController.bpost_put_unpublish);
 
 module.exports = router;
